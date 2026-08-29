601f1e6656ceda0518937221e1ae7e71085a1ebe
mh1640597-ctrl <mh1640597@gmail.com>
2026-08-29 17:37:58 +0500

    Add files via upload
    first class assignment upload

---
+++ main1.py
@@ -0,0 +1,202 @@
+name = "Ali"
+age = " 19"
+city = "karachi"
+student = True
+print(name)
+print(age)
+print(city)
+print(student)
+print(name, type(name))
+print(age,type(age))
+print(marks,type(marks))
+print(student, type(student))
+name = "Ali"
+age = 18
+marks = 80.7
+student = True
+print(name,type(name))
+print(age,type(age))
+print(marks,type(marks))
+print(student,type(student))
+a = 10
+b = 3
+print("Addition =", a + b)
+print("subtraction", a - b)
+print("division =" a / b)
+print("power =", a**b)
+print("multiplication =", a* b)
+print("modulus =", a % b)
+a = 10
+b = 3
+print("Addition =", a + b)
+print("subtraction", a - b)
+print("division =", a / b)
+print("power =", a**b)
+print("multiplication =", a* b)
+print("modulus =", a % b)
+english = 70 
+math = 80
+computer = 89
+total = english + math+ computer 
+average = total / 3
+print ("Total marks =", total)
+print("average marks =", average)
+price = 500
+quantity = 3
+ bill = price * quantity
+print("price =", price)
+print("quantity =", quantity)
+print("total bill =", bill)
+print(" price =", price)
+print("quantity =", quantity)
+print("total bill =", bill)
+price = 500
+quantity = 3
+bill = price * quantity
+print(" price =", price)
+print("quantity =", quantity)
+print("total bill =", bill)
+length = 10
+width = 5 
+ area = length * width 
+ perimeter = 2 * (length +
+length = 10
+width = 5 
+ area = length * width 
+ perimeter = 2 * (length + width)
+
+print("area =", area)
+print("perimeter =", perimeter)
+length = 10
+width = 5 
+ area = length * width 
+ perimeter = 2 * (length + width)
+length = 10
+width = 5 
+
+ area = length * width 
+ perimeter = 2 * (length + width)
+length = 10
+width = 5 
+
+ area = length * width 
+ perimeter = 2 * (length + width)
+
+print("area =", area)
+print("perimeter =", perimeter)
+minutes = 250
+
+hours = minutes //60
+remaining = minutes % 60
+
+print("hours =", hours)
+print("remaining minutes =", remaining)
+marks = 60
+attendence = 80
+
+eligible = marks >= 50 and attendence  >=75
+
+print("eligible =", eligible)
+a = true
+b = false 
+
+print (a and b)
+print(a or b)'
+print(not a
+a = true
+b = false 
+
+print (a and b)
+print(a or b)'
+print(not a)
+print(not b)
+a = true
+b = false 
+
+print (a and b)
+print(a or b)
+print(not a)
+print(not b)
+a = true
+b = false 
+
+print (a and b)
+print(a or b)
+print(not a)
+print(not b)
+a = true
+b = false 
+print (a and b)
+print(a or b)
+print(not a)
+print(not b)
+a = True
+b = False 
+print (a and b)
+print(a or b)
+print(not a)
+print(not b)
+name = "Ali"
+age = 18
+
+english = 70
+math = 80
+computer = 75
+
+attendance = 80
+
+total = english + math + computer 
+average = total / 3
+
+eligible = average >= 50 and 
+attendence >= 75 
+
+print("name:", name)
+print("age:", age)
+print("english:", english)
+print("math:", math)
+print("computer:", computer)
+print("attendence :", attendence)
+print("total marks:", total)
+print("average marks:", average)
+print("eligible :", eligible)
+name = "Ali"
+age = 18
+
+english = 70
+math = 80
+computer = 75
+
+attendance = 80
+
+total = english + math + computer 
+average = total / 3
+
+eligible = average >= 50 and attendence >= 75 
+
+print("name:", name)
+print("age:", age)
+print("english:", english)
+print("math:", math)
+print("computer:", computer)
+print("attendence :", attendence)
+print("total marks:", total)
+print("average marks:", average)
+print("eligible :", eligible)
+length = 10
+width = 5 
+
+ Area = length * width 
+ Perimeter = 2 * (length + width)
+
+print("Area =", Area)
+print("Perimeter =", Perimeter)
+length = 10
+width = 5 
+
+Area = length * width 
+Perimeter = 2 * (length + width)
+
+print("Area =", Area)
+print("Perimeter =", Perimeter)
+%history -f main1.py
